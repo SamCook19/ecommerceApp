5795dcbe7349589ff2d24d4c789693231ecbd443
Sam Cook <sam.cook@suusuccess.org>
2020-12-14 14:52:18 -0700

    finished navbar feature

--- src/actions/headernavbar.js
+++ src/actions/headernavbar.js
@@ -1,10 +1,9 @@
 
-
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from './types';
-
 
 export function setHeaderLinks(links) {
     return ({
@@ -17,5 +16,12 @@ export function setNavbarLinks(links) {
     return ({
         type: SET_NAVBAR_LINKS,
         payload: links
+    })
+}
+
+export function changeNavbarActive(_id) {
+    return ({
+        type: CHANGE_NAVBAR_ACTIVE,
+        payload: _id
     })
 }
