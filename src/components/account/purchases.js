e93a08de7205ce767d226c8920734ba042df3086
Sam Cook <sam.cook@suusuccess.org>
2020-12-15 15:31:01 -0700

    finished purchases component

--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -13,13 +13,17 @@ class Purchases extends Component {
             const { className } = this.props;
 
             return (
-            <div className={`${className}purchases`}> 
-                {
-                    this.props.purchases.map(purchase => {
-                        return <div> {purchase.title} </div>
-                    })
-                }
-            </div>
+                <div className={`${className} purchases`}>
+                    {
+                        this.props.purchases.map(purchase => {
+                            return (
+                                <div key={purchase._id} className='purchases__purchase purchase'>
+                                    <img className='purchase__img' src='http://via.placeholder.com/80x80'/>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             )
     }
 }
