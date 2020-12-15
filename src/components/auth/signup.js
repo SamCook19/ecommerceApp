36536a9620a13c9706d039411c1551806276f38c
Sam Cook <sam.cook@suusuccess.org>
2020-12-15 13:54:08 -0700

    setup purchase history component

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 
 import SignUpForm from './signupForm';
-import PageTitle from '../pageTitle';
+import PageTitle from '../pagetitle';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
